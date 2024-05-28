925954c471ec9303f2816da99726c19f013449fb
raz <vicevasquez95@gmail.com>
2024-05-28 16:59:20 -0400

    modify : test of packages install, and add automatic setup config

--- setup.sh
+++ setup.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-# Paquetes esenciales
-sudo pacman -S --noconfirm --needed base-devel git obsidian discord firefox starship waybar neofetch ttf-firacode-nerd thunar nwg-look pamixer brightnessctl
+# Paquetes esenciales (Tested)
 
-# AUR Helper
+sudo pacman -Syu --noconfirm base-devel gnome-keyring obsidian discord firefox starship waybar neofetch ttf-firacode-nerd thunar nwg-look pamixer brightnessctl alacritty github-cli
+
+# AUR Helper (Tested)
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-# Paquetes AUR
-yay -S --noconfirm --needed alacritty dracula-gtk-theme-git dracula-icon-theme-git dracula-cursor-theme-git swww github-desktop-bin octopi ulauncher github-cli nodejs npm libreoffice-still-es google-chrome gitkraken postman-bin visual-studio-code-bin
+# Paquetes AUR (Tested)
+yay -Sy --noconfirm swww github-desktop-bin octopi ulauncher nodejs npm libreoffice-still-es google-chrome gitkraken postman-bin visual-studio-code-bin dracula-gtk-theme-git dracula-icons-git dracula-cursors-git
 
-# Starship Config
+## Config Hyprdots
+cp -rf ~/.config-arch/hypr/ ~/.config/
 
+## Configuración de Alacritty
+cp -rf ~/.config-arch/alacritty/ ~/.config/
+
+## Configuración de Waybar
+cp -rf ~/.config-arch/waybar ~/.config/
+
+# ZSH and Starship
+
+## ZSH config
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+cp -rf ~/.config-arch/zshrc ~/.zshrc
+
+## ZSH plugins
+cd ~/.oh-my-zsh/custom/plugins
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions
+sudo git clone https://github.com/lukechilds/zsh-nvm
+
+
+## Starship Config
 curl -sS https://starship.rs/install.sh | sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
+
+# GTK config
+cp -rf ~/.config-arch/gtk-3.0/ ~/.config/
+cp -rf ~/.config-arch/xsettingsd/ ~/.config/
+cp -rf ~/.config-arch/.gtkrc-2.0 ~/
+
